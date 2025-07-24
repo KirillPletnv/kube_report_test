df6a5aaab764a3a33d05ba2513ecafa5e19169b4
Kirill <kirillfromhome@gmail.com>
2025-07-24 10:58:04 +0300

    Update krakozyabros test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ URL="https://raw.githubusercontent.com/GreatMedivack/files/master/list.out"
 ORIG_FILE="temp_list.out"
 wget -q "$URL" -O "$ORIG_FILE"
 
-#Create server name (РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ "default_server")
+#Create server name (default is "default_server")
 SERVER_NAME="${1:-default_server}"
 DATE=$(date +"%d_%m_%Y")
 
@@ -16,12 +16,12 @@ REPORT_FILE="${SERVER_NAME}_${DATE}_report.out"
 ARCHIVE_FILE="${SERVER_NAME}_${DATE}.tar.gz"
 ARCHIVE_DIR="archives"
 
-#Create services file with erors (Error РёР»Рё CrashLoopBackOff)
+#Create services file with errors (Error or CrashLoopBackOff)
 grep -E 'Error|CrashLoopBackOff' "$ORIG_FILE" | \
 awk '{print $1}' | \
 sed -E 's/(-[^-]{6,10}-[^-]{5,6})$//' > "$FAILED_FILE"
 
-#Create runing services file (Running)
+#Create running services file (Running)
 grep 'Running' "$ORIG_FILE" | \
 awk '{print $1}' | \
 sed -E 's/(-[^-]{6,10}-[^-]{5,6})$//' > "$RUNNING_FILE"
@@ -32,10 +32,10 @@ FAILED_COUNT=$(wc -l < "$FAILED_FILE")
 USERNAME=$(whoami)
 CURRENT_DATE=$(date +"%d/%m/%y")
 {
-    echo "РљРѕР»РёС‡РµСЃС‚РІРѕ СЂР°Р±РѕС‚Р°СЋС‰РёС… СЃРµСЂРІРёСЃРѕРІ: $RUNNING_COUNT"
-    echo "РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРµСЂРІРёСЃРѕРІ СЃ РѕС€РёР±РєР°РјРё: $FAILED_COUNT"
-    echo "РРјСЏ СЃРёСЃС‚РµРјРЅРѕРіРѕ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ: $USERNAME"
-    echo "Р”Р°С‚Р°: $CURRENT_DATE"
+    echo "Number of running services: $RUNNING_COUNT"
+    echo "Number of failed services: $FAILED_COUNT"
+    echo "System username: $USERNAME"
+    echo "Date: $CURRENT_DATE"
 } > "$REPORT_FILE"
 
 #Create access
@@ -53,9 +53,9 @@ rm -f "$ORIG_FILE" "$FAILED_FILE" "$RUNNING_FILE" "$REPORT_FILE"
 
 #Archive check
 if tar -tzf "${ARCHIVE_DIR}/${ARCHIVE_FILE}" >/dev/null 2>&1; then
-    echo "РђСЂС…РёРІ СЃРѕР·РґР°РЅ СѓСЃРїРµС€РЅРѕ: ${ARCHIVE_DIR}/${ARCHIVE_FILE}"
+    echo "Archive created successfully: ${ARCHIVE_DIR}/${ARCHIVE_FILE}"
     exit 0
 else
-    echo "РћС€РёР±РєР°: Р°СЂС…РёРІ РїРѕРІСЂРµР¶РґРµРЅ" >&2
+    echo "Error: archive is corrupted" >&2
     exit 1
 fi
